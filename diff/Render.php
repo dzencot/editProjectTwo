23ab608aa407890e8ae46a58bc536db619f6bb3a
Konstantin <29775482+lobr17@users.noreply.github.com>
2020-08-11 18:34:17 +0300

    Update Render.php

--- diff/Render.php
+++ diff/Render.php
@@ -9,17 +9,17 @@ function render($array)
     $result = array_map(function ($child) {
         if ($child['type'] === '-') {
            // var_dump($child["children"]);
-            return [$child['type'], $child['name'], $child['children']];
+            return "{$child['type']}, {$child['name']}, {$child['children']}";
         } elseif ($child['type'] === '+') {
-            return [$child['type'], $child['name'], $child['children']];
+            return "{$child['type']}, {$child['name']}, {$child['children']}";
         } elseif ($child['type'] === 'has not changed') {
-            return [$child['type'], $child['name'], $child['children']];
+            return "{$child['type']}, {$child['name']}, {$child['children']}";
         } elseif ($child['type'] === 'changed') {
-            return [$child['type'], $child['name'], $child['children']];
+            return "{$child['type']}, {$child['name']}, {$child['children']}";
         } elseif ($child['type'] === 'nested') {
             $result = array_map(function ($subChild) {
                 if (is_array($subChild)) {
-                    return [render($subChild)];
+                    return render($subChild);
                 }
             }, $child);
             return $result;
