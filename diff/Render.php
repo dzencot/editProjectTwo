2795745ee5d73fd81d0281edde9b65d183eee60a
Konstantin <29775482+lobr17@users.noreply.github.com>
2020-08-11 20:41:16 +0300

    Update Render.php

--- diff/Render.php
+++ diff/Render.php
@@ -8,7 +8,6 @@ function render($array)
 {
     $result = array_map(function ($child) {
         if ($child['type'] === '-') {
-           // var_dump($child["children"]);
             return "{$child['type']}, {$child['name']}, {$child['children']}";
         } elseif ($child['type'] === '+') {
             return "{$child['type']}, {$child['name']}, {$child['children']}";
@@ -17,18 +16,16 @@ function render($array)
         } elseif ($child['type'] === 'changed') {
             return "{$child['type']}, {$child['name']}, {$child['children']}";
         } elseif ($child['type'] === 'nested') {
+            
             $result = array_map(function ($subChild) {
                 if (is_array($subChild)) {
                     return render($subChild);
                 }
             }, $child);
+            
             return $result;
         }
-
-        //print_r($child);
        // print_r(nl2br(PHP_EOL));
-
-
     }, $array);
 
     return $result;
