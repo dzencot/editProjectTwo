df40846ab6c32bfab3add4761359020390e36db9
Konstantin <29775482+lobr17@users.noreply.github.com>
2020-08-11 12:03:24 +0300

    Update DiffArray.php

--- diff/DiffArray.php
+++ diff/DiffArray.php
@@ -15,22 +15,22 @@ function diffArray ($array1, $array2, $option = null)
     $result = array_map(function ($key) use ($array1, $array2, $keys1, $keys2, $option) {
         //ключ есть только в before.
         if (!in_array($key, $keys2)) {
-            return ['name' => $key, 'type' => '-'];
+            return ['name' => $key, 'type' => '-', 'children' => $array1[$key]];
         //ключ есть только в after.
         } elseif (!in_array($key, $keys1)) {
-            return ['name' => $key, 'type' => '+'];
+            return ['name' => $key, 'type' => '+', 'children' => $array2[$key]];
         //одинаковые ключи.
         } elseif (in_array($key, $keys1) and in_array($key, $keys2)) {
             //значения НЕ объекты.
             if (!is_array($array1[$key]) and !is_array($array2[$key])) {
                 if ($array1[$key] === $array2[$key]) {
-                    return ['name' => $key, 'type' => 'has not changed'];
+                    return ['name' => $key, 'type' => 'has not changed', 'children' => $array1[$key]];
                 } elseif ($array1[$key] !== $array2[$key]) {
-                    return ['name' => $key, 'type' => '-' . $array1[$key] . ' +' . $array2[$key]];
+                    return ['name' => $key, 'type' => 'changed', 'children' => '-' . $array1[$key] . ' +' . $array2[$key]];
                 }
              //значения объекты.
             } elseif (is_array($array1[$key]) and is_array($array2[$key])) {
-                return [$key => diffArray($array1[$key], $array2[$key]), 'type' => 'object'];
+                return [$key => diffArray($array1[$key], $array2[$key]), 'type' => 'nested'];
             }
         }
     }, $fish);
