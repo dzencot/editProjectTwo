388c83d52c40c130d5eb51201f6dde2ffe71c32e
Konstantin <29775482+lobr17@users.noreply.github.com>
2020-08-17 17:56:41 +0300

    Update DiffArray.php

--- diff/DiffArray.php
+++ diff/DiffArray.php
@@ -15,27 +15,80 @@ function diffArray ($array1, $array2, $option = null)
     $result = array_map(function ($key) use ($array1, $array2, $keys1, $keys2, $option) {
         //ключ есть только в before.
         if (!in_array($key, $keys2)) {
-            return ['name' => $key, 'type' => '-', 'children' => $array1[$key]];
+
+            if (is_array($array1[$key])) {
+                return ['name' => $key, 'type' => '-', 'children' => array_keys($array1[$key])['0'] . ':' . dfs($key, $array1[$key])];
+            }
+
+            // Временно закрыл
+            //return ['name' => $key, 'type' => '-', 'children' => $array1[$key]];
+
         //ключ есть только в after.
         } elseif (!in_array($key, $keys1)) {
+            if (is_array($array2[$key])) {
+                return ['name' => $key, 'type' => '-', 'children' => array_keys($array2[$key])['0'] . ':' . dfs($key, $array2[$key])];
+            }
+
             return ['name' => $key, 'type' => '+', 'children' => $array2[$key]];
-        //одинаковые ключи.
+
+        // Одинаковые ключи
         } elseif (in_array($key, $keys1) and in_array($key, $keys2)) {
-            //значения НЕ объекты.
+            // Значения строки
             if (!is_array($array1[$key]) and !is_array($array2[$key])) {
                 if ($array1[$key] === $array2[$key]) {
                     return ['name' => $key, 'type' => ' ', 'children' => $array1[$key]];
+                    //РЫБА--------------------------------------
                 } elseif ($array1[$key] !== $array2[$key]) {
-                    return ['name' => $key, 'type' => 'changed', 'children' => '- ' . $key . ': ' . $array1[$key] . nl2br(PHP_EOL) . '+ ' . $key . ': ' . $array2[$key]];
+                    return ['name' => $key, 'type' => 'changed', 'childrenMinus' => '- ' . $key . ': ' . $array1[$key], 'childrenPlus' => '+ ' . $key . ': ' . $array2[$key]];
                 }
-             //значения объекты.
+            // Значения объекты
             } elseif (is_array($array1[$key]) and is_array($array2[$key])) {
                 return [$key => diffArray($array1[$key], $array2[$key]), 'type' => 'nested'];
+            // Значения объект и строка
+            } elseif (is_array($array1[$key]) and !is_array($array2[$key]) or !is_array($array1[$key]) and is_array($array2[$key])) {
+                if (is_array($array1[$key]) and !is_array($array2[$key])) {
+                    return ['name' => $key, 'type' => 'changed', 'childrenMinus' => '- ' . $key . ': ' . array_keys($array1[$key])['0'] . ':' . dfs($key, $array1[$key]), 'childrenPlus' => '+ ' . $key . ': ' . $array2[$key]];
+                } elseif (!is_array($array1[$key]) and is_array($array2[$key])) {
+                    return ['name' => $key, 'type' => 'changed', 'childrenMinus' => '- ' . $key . ': ' . $array1[$key], 'childrenPlus' => '+ ' . $key . ': ' . array_keys($array2[$key])['0'] . ':' . dfs($key, $array2[$key])];
+                }
             }
         }
     }, $fish);
     return $result;
 }
+
+// Вложенный массив в строку.
+function dfs($key, $subArray) {
+    $keys = array_keys($subArray);
+
+    $resultRender = array_reduce($subArray, function ($acc, $child) use ($key, $keys) {
+
+        // Пробую пробежаться по ключам
+      /*  array_map(function ($subKeys) use ($child, $key) {
+            //if ($child) {
+            print_r($subKeys);
+            //print_r(nl2br(PHP_EOL));
+            //print_r($subKeys);
+
+            print_r(nl2br(PHP_EOL));
+        //}
+        }, $keys);*/
+
+        if (is_array($child)) {
+            $acc .= array_keys($child)['0'] . ': {' . nl2br(PHP_EOL) . dfs($key, $child)  .  '}' . nl2br(PHP_EOL);
+            return $acc;
+        }
+        $acc .=  $child . nl2br(PHP_EOL);
+        return $acc;
+    }, '');
+
+
+    return $resultRender;
+
+}
+
+
+
 
 
 
