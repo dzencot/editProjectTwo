c07aec441bb8b02444bb32bba83cec9d0f69de0d
Konstantin <29775482+lobr17@users.noreply.github.com>
2020-08-19 14:05:28 +0300

    Update Render.php

--- diff/Render.php
+++ diff/Render.php
@@ -5,52 +5,33 @@ namespace Dif\Dif\Render;
 require_once __DIR__ . '/../vendor/autoload.php';
 
 
-
 function render($array, $depth)
 {
     //Делаем читаемый массив.
     $result = array_map(function ($child) use ($depth) {
-        if ($child['type'] === '-') {
-           // var_dump($child['children']);
-            print_r(nl2br(PHP_EOL));
-
-            if (!is_array($child['children'])) {
-                return "{$child['type']}  {$child['name']}: {$child['children']}";
-            } elseif (is_array($child['children'])) {
-                return "{$child['type']} {$child['name']}: " . "{" . nl2br(PHP_EOL) . str_repeat('&nbsp;', $depth + 5 ) . implode(array_keys($child['children'])) . ": " . implode($child['children']) . nl2br(PHP_EOL) . str_repeat('&nbsp;', $depth ) . "}";
-            }
-        } elseif ($child['type'] === '+') {
-            if (!is_array($child['children'])) {
-                return "{$child['type']} {$child['name']}: {$child['children']}";
-
-            } elseif (is_array($child['children'])) {
-                return "{$child['type']} {$child['name']}: " . "{" . nl2br(PHP_EOL) . str_repeat('&nbsp;', $depth + 5 ) . implode(array_keys($child['children'])) . ": " . implode($child['children']) . nl2br(PHP_EOL) . str_repeat('&nbsp;', $depth ) . "}";
-            }
-            // Одинаковые значения
-        } elseif ($child['type'] === ' ') {
-            if (!is_array($child['children'])) {
-                return "&nbsp; {$child['type']} {$child['name']}: {$child['children']}";
-            } elseif (is_array($child['children'])) {
-                return "{$child['type']} {$child['name']}: " . "{" . nl2br(PHP_EOL) .  implode(array_keys($child['children'])) . ": " . implode($child['children']) . nl2br(PHP_EOL) . str_repeat('&nbsp;', $depth ) . "}";
-            }
+        $formattedValue = getFormattedValue($child, $depth);
+        if ($child['type'] === 'removed') {
+           return "- " . $formattedValue;
+        } elseif ($child['type'] === 'add') {
+           return "+ " . $formattedValue;
+        // Одинаковые значения
+        } elseif ($child['type'] === 'unchanged') {
+           return str_repeat('&nbsp;', 3) . $formattedValue;
         // Разные значения
         } elseif ($child['type'] === 'changed') {
-            // Строки
-            if (!is_array($child['children'])) {
-
-                return "{$child['childrenMinus']}" . nl2br(PHP_EOL) . str_repeat('&nbsp;', $depth ) . "{$child['childrenPlus']}";
             // Объекты
-            } elseif (is_array($child['children'])) {
-
-                return "{$child['type']} {$child['name']}: " . "{" . nl2br(PHP_EOL) . implode(array_keys($child['children'])) . ": " . implode($child['children']) . nl2br(PHP_EOL) . str_repeat('&nbsp;', $depth ) . "}";
-            // Объект и строка
+            if (is_array($child['value'])) {
+                return "{$child['type']} {$child['name']}: " . "{" . nl2br(PHP_EOL) . implode(array_keys($child['value'])) . ": " . implode($child['value']) . nl2br(PHP_EOL) . str_repeat('&nbsp;', $depth ) . "}";
             }
-
+            // Строки
+            return "- {$child['oldValue']}" . nl2br(PHP_EOL) . str_repeat('&nbsp;', $depth) . "+ {$child['newValue']}";
+        // Объект и строка
         } elseif ($child['type'] === 'nested') {
             unset($child['type']);
 
             $result = array_map(function ($subChild) use ($child, $depth) {
                 if (is_array($subChild)) {
+                    //Пробел для Settinga6 НО при этом и других берет
                     return render($subChild, $depth + 5);
                 }
             }, $child);
@@ -77,6 +58,16 @@ function renderWithDepth($array) {
     return render($array, 0);
 }
 
+function getFormattedValue($child, $depth) {
+    if (is_array($child['value'])) {
+        return "{$child['value']}: " . "{" . nl2br(PHP_EOL) . str_repeat('&nbsp;', $depth + 5 ) . implode(array_keys($child['value'])) . ": " . implode($child['value']) . nl2br(PHP_EOL) . str_repeat('&nbsp;', $depth ) . "}";
+    }
+    return "{$child['name']}:" . " {$child['value']}" ;
+}
+
+function makeIndent($depth) {
+    return str_repeat('&nbsp;', $depth);
+}
 
 
 
